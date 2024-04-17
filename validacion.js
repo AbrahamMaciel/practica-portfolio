587c4dedf3b7acaa3ce200305a70366751558ebf
Abraham Maciel <fernandezabmaciel@gmail.com>
2024-04-17 00:36:14 -0300

    Hecho mensaje de error basico para las validaciones
    Falta implementar js

--- validacion.js
+++ validacion.js
@@ -1,1 +1,37 @@
-//Haz tú validación en javascript acá
+ //Haz tú validación en javascript acá
+let formcontacto = document.querySelector('.formcontacto__form');
+
+formcontacto.addEventListener('submit' , (e) => {
+    let mensajes = [];
+    
+    e.preventDefault();
+    validarNombre(mensajes);
+    // validarEmail();
+    // validarAsunto();
+    // validarMensaje();
+
+});
+
+// Esto Valida que: 
+// - El campo nombre no este vacio o en blanco
+// - Que tenga como maximo 50 caracteres
+
+function validarNombre(mensajes){
+    let nombre = document.getElementById('nombre');
+
+    if (nombre.value === '' || nombre.value === null){
+        mensajes.push('Se requiere un nombre pue');
+        console.log(mensajes.pop());
+    }
+
+    if(nombre.value.length >= 5){
+        mensajes.push('El nombre debe ser de menos de 40 caracteres');
+        console.log(mensajes.pop());
+    }
+}
+
+// Esto deberia agregar un mensaje en el bloque de error
+function mostrarError(){
+
+}
+
