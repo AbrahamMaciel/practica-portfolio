1c6f72d12792378ae22480743fb3b68b50fa58fe
Abraham Maciel <fernandezabmaciel@gmail.com>
2024-04-17 14:58:04 -0300

    Hecha animacion de muestra de errores basica.
    Ahora se muestra con una pequena transicion, trabajando en el JS para hacer las funciones de validacion modulares.

--- validacion.js
+++ validacion.js
@@ -6,11 +6,20 @@ formcontacto.addEventListener('submit' , (e) => {
     
     e.preventDefault();
     validarNombre(mensajes);
-    // validarEmail();
+    validarEmail(mensajes);
     // validarAsunto();
     // validarMensaje();
 
 });
+
+function validarCampoVacio(idTarget, mensajes){
+    let target = document.getElementById(idTarget);
+
+    if(target.value === '' || target.value === null){
+        mensajes.push(`Se requiere un ${idTarget}.`);
+        mostrarError(mensajes.pop());
+    }
+}
 
 // Esto Valida que: 
 // - El campo nombre no este vacio o en blanco
@@ -19,19 +28,30 @@ formcontacto.addEventListener('submit' , (e) => {
 function validarNombre(mensajes){
     let nombre = document.getElementById('nombre');
 
-    if (nombre.value === '' || nombre.value === null){
-        mensajes.push('Se requiere un nombre pue');
-        console.log(mensajes.pop());
-    }
+    validarCampoVacio('nombre', mensajes);
 
-    if(nombre.value.length >= 5){
-        mensajes.push('El nombre debe ser de menos de 40 caracteres');
-        console.log(mensajes.pop());
+    if(nombre.value.length >= 50){
+        mensajes.push('El nombre debe tener como maximo 50 caracteres.');
+        mostrarError(mensajes.pop());
     }
 }
 
-// Esto deberia agregar un mensaje en el bloque de error
-function mostrarError(){
+// Esto Valida que: 
+// - El campo nombre no este vacio o en blanco
+// - Deber estar en formato e-mail conteniendo el caracter especial @ seguido de un dominio o proveedor seguido de un punto(.)
+function validarEmail(mensajes){
+    validarCampoVacio('email', mensajes);
+}
 
+// Esto deberia agregar un paragrafo con mensaje en el bloque de error
+function mostrarError(mensaje){
+    let errorContainer = document.querySelector('.formcontacto__error__message');
+    let errorParagraph = document.createElement('p');
+    errorParagraph.classList.add('error__message__item');
+    errorParagraph.innerHTML = mensaje;
+    errorContainer.append(errorParagraph);
+
+    errorContainer.classList.add('show');
+    errorContainer.scrollIntoView({behavior: 'smooth'});    
 }
 
