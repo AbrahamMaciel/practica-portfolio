903800b3d310f7b7f84c052122c7c5b0d41bfb6e
Abraham Maciel <fernandezabmaciel@gmail.com>
2024-04-17 22:14:54 -0300

    Retrocediendo ultimos cambios, nueva forma de mostrar errores semi-hecha
    Eso me pasa por no leer todos los requisitos, me re cabio con lo del boton de enviar.

--- validacion.js
+++ validacion.js
@@ -1,57 +1,66 @@
  //Haz tú validación en javascript acá
-let formcontacto = document.querySelector('.formcontacto__form');
 
-formcontacto.addEventListener('submit' , (e) => {
-    let mensajes = [];
+ function hola(){
+    console.log('UCHIKUDAKE');
+ }
+
+ function viteh(){
+    console.log('TOKI MICHITE!');
+ }
+
+
+// let formcontacto = document.querySelector('.formcontacto__form');
+
+// formcontacto.addEventListener('submit' , (e) => {
+//     let mensajes = [];
     
-    e.preventDefault();
-    validarNombre(mensajes);
-    validarEmail(mensajes);
-    // validarAsunto();
-    // validarMensaje();
+//     e.preventDefault();
+//     validarNombre(mensajes);
+//     validarEmail(mensajes);
+//     // validarAsunto();
+//     // validarMensaje();
 
-});
+// });
 
-function validarCampoVacio(idTarget, mensajes){
-    let target = document.getElementById(idTarget);
+// function validarCampoVacio(idTarget, mensajes){
+//     let target = document.getElementById(idTarget);
 
-    if(target.value === '' || target.value === null){
-        mensajes.push(`Se requiere un ${idTarget}.`);
-        mostrarError(mensajes.pop());
-    }
-}
+//     if(target.value === '' || target.value === null){
+//         mensajes.push(`Se requiere un ${idTarget}.`);
+//         mostrarError(mensajes.pop());
+//     }
+// }
 
-// Esto Valida que: 
-// - El campo nombre no este vacio o en blanco
-// - Que tenga como maximo 50 caracteres
+// // Esto Valida que: 
+// // - El campo nombre no este vacio o en blanco
+// // - Que tenga como maximo 50 caracteres
 
-function validarNombre(mensajes){
-    let nombre = document.getElementById('nombre');
+// function validarNombre(mensajes){
+//     let nombre = document.getElementById('nombre');
 
-    validarCampoVacio('nombre', mensajes);
+//     validarCampoVacio('nombre', mensajes);
 
-    if(nombre.value.length >= 50){
-        mensajes.push('El nombre debe tener como maximo 50 caracteres.');
-        mostrarError(mensajes.pop());
-    }
-}
+//     if(nombre.value.length >= 50){
+//         mensajes.push('El nombre debe tener como maximo 50 caracteres.');
+//         mostrarError(mensajes.pop());
+//     }
+// }
 
-// Esto Valida que: 
-// - El campo nombre no este vacio o en blanco
-// - Deber estar en formato e-mail conteniendo el caracter especial @ seguido de un dominio o proveedor seguido de un punto(.)
-function validarEmail(mensajes){
-    validarCampoVacio('email', mensajes);
-}
+// // Esto Valida que: 
+// // - El campo nombre no este vacio o en blanco
+// // - Deber estar en formato e-mail conteniendo el caracter especial @ seguido de un dominio o proveedor seguido de un punto(.)
+// function validarEmail(mensajes){
+//     validarCampoVacio('email', mensajes);
+// }
 
-// Esto deberia agregar un paragrafo con mensaje en el bloque de error
-function mostrarError(mensaje){
-    let errorContainer = document.querySelector('.formcontacto__error__message');
-    let errorParagraph = document.createElement('p');
-    errorParagraph.classList.add('error__message__item');
-    errorParagraph.innerHTML = mensaje;
-    errorContainer.append(errorParagraph);
+// // Esto deberia agregar un paragrafo con mensaje en el bloque de error
+// function mostrarError(mensaje){
+//     let errorContainer = document.querySelector('.formcontacto__error__message');
+//     let errorParagraph = document.createElement('p');
+//     errorParagraph.classList.add('error__message__item');
+//     errorParagraph.innerHTML = mensaje;
+//     errorContainer.append(errorParagraph);
 
-    errorContainer.classList.add('show');
-    errorContainer.scrollIntoView({behavior: 'smooth'});    
-}
-
+//     errorContainer.classList.add('show');
+//     errorContainer.scrollIntoView({behavior: 'smooth'});    
+// }
